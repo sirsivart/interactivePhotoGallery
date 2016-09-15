3e458e983c618e38535e786781f12696e524460e
travis carney <traviscarney@gmail.com>
2016-09-14 22:53:33 -0400

    Caption and Arrows
    Added the alt tag caption to the light box.  Started styling the
    previous and next arrows to appear properly.

--- js/featherlight.gallery.js
+++ js/featherlight.gallery.js
@@ -102,8 +102,8 @@
 
 	$.extend(FeatherlightGallery.prototype, {
 		/** Additional settings for Gallery **/
-		previousIcon: '&#9664;',     /* Code that is used as previous icon */
-		nextIcon: '&#9654;',         /* Code that is used as next icon */
+		previousIcon: '&#8249;',     /* Code that is used as previous icon */
+		nextIcon: '&#8250;',         /* Code that is used as next icon */
 		galleryFadeIn: 100,          /* fadeIn speed when image is loaded */
 		galleryFadeOut: 300,         /* fadeOut speed before image is loaded */
 
--- js/main.js
+++ js/main.js
@@ -7,10 +7,13 @@ $(document).ready(function() {
     $('.image').featherlight({
     });
 });
+$.featherlightGallery.prototype.afterContent = function() {
+  var caption = this.$currentTarget.find('img').attr('alt');
+  this.$instance.find('.caption').remove();
+  $('<div class="caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
+};
 $('a.image').featherlightGallery({
     type: 'image',
-    previousIcon: '«',
-    nextIcon: '»',
     galleryFadeIn: 300,
 
     openSpeed: 300
